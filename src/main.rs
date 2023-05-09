5a5bd8bf4162e6a2e05875c6e85b0ec19095374d
coufxr <mu993364@163.com>
2023-05-09 17:40:27 +0800

    fix: 可以四则的计算器

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,10 @@ slint::include_modules!();
 
 #[derive(Default)]
 struct CalcState {
-    prev_value: slint::SharedString,
-    current_value: slint::SharedString,
+    prev_value: i32,
+    current_value: i32,
     operator: slint::SharedString,
 }
-
-// fn join_str(mut state: std::cell::RefMut<CalcState>, val: i32) -> std::cell::RefMut<'_, CalcState> {
-//     let  Ok(mut a) = state.prev_value.parse::<i32>() else { todo!() };
-    
-//     a *= 10;
-//     a += val;
-//     state.current_value=slint::SharedString::from(a.to_string().as_str());
-//     state
-// }
-
-// fn calc_str(
-//     mut state: std::cell::RefMut<'_, CalcState>,
-// ) -> (std::cell::RefMut<'_, CalcState>, i32) {
-//     let Ok(a) = state.prev_value.parse::<i32>() else { todo!() };
-//     let Ok(b) = state.current_value.parse::<i32>() else { todo!() };
-//     let rusult = match state.operator.as_str() {
-//         "+" => a+b,
-//         "-" => a-b,
-//         "*" => a*b,
-//         "/" => a/b,
-//         &_ => todo!(),
-//     };
-//     state.current_value = Default::default();
-//     state.operator = Default::default();
-//     (state, rusult)
-// }
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
@@ -47,29 +21,33 @@ fn main() -> Result<(), slint::PlatformError> {
         let mut state: std::cell::RefMut<CalcState> = state.borrow_mut();
         match value.parse::<i32>() {
             Ok(val) => {
-                let  Ok(mut a) = state.prev_value.parse::<i32>() else { todo!() };
-                a *= 10;
-                a += val;
-                state.current_value=slint::SharedString::from(a.to_string().as_str());
+                state.current_value *= 10;
+                state.current_value += val;
+                // app.set_value(state.current_value);
+                app.set_value(slint::SharedString::from(
+                    state.current_value.to_string().as_str(),
+                ));
             }
             Err(_) => {
-                let Ok(a) = state.prev_value.parse::<i32>() else { todo!() };
-                let Ok(b) = state.current_value.parse::<i32>() else { todo!() };
-                let rusult = match state.operator.as_str() {
-                    "+" => a+b,
-                    "-" => a-b,
-                    "*" => a*b,
-                    "/" => a/b,
-                    &_ => todo!(),
-                };
-                state.current_value = Default::default();
-                state.operator = Default::default();
-                app.set_value(slint::SharedString::from(rusult.to_string().as_str()));
-            },
+                if value.as_str() == "=" {
+                    let rusult = match state.operator.as_str() {
+                        "+" => state.prev_value + state.current_value,
+                        "-" => state.prev_value - state.current_value,
+                        "*" => state.prev_value * state.current_value,
+                        "/" => state.prev_value / state.current_value,
+                        _ => state.current_value,
+                    };
+                    // app.set_value(rusult);
+                    app.set_value(slint::SharedString::from(rusult.to_string().as_str()));
+                    state.current_value = 0;
+                    state.operator = Default::default();
+                } else {
+                    state.operator = value.clone();
+                    state.prev_value = state.current_value;
+                    state.current_value = 0;
+                }
+            }
         };
-
-        let value = &value;
-        app.set_value(app.get_value() + value);
     });
     ui.run()
 }
